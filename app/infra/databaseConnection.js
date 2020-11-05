d89a04f37ce9166136d16d302762ebea3587baec
William Libero <williamliberos@gmail.com>
2020-11-04 21:09:01 -0300

    Correçao do problema de conexao no bd

--- app/infra/databaseConnection.js
+++ app/infra/databaseConnection.js
@@ -1,18 +1,32 @@
 var mysql      = require('mysql');
-var connection = mysql.createConnection({
+var connection = {
   host     : 'br182.hostgator.com.br',
   user     : 'evidence_dev',
   password : 'evidence_dev1',
   database : 'evidence_artha'
-});
- 
-connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
- 
-  console.log('connected as id ' + connection.threadId);
-});
+};
+
+function handleDisconnect() {
+  connection = mysql.createConnection(connection);
+  
+  connection.connect(function(err) {
+    if(err) {
+      console.error('error connecting: ' + err.stack);
+      setTimeout(handleDisconnect, 2000);
+    }
+
+    console.log('connected as id ' + connection.threadId);
+  });
+
+  connection.on('error', function(err) {
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
 
 module.exports = connection;
