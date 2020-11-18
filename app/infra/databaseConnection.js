255f67bc3a4501177a48dd16c427281a61f3b61c
William Libero <williamliberos@gmail.com>
2020-11-17 21:21:52 -0300

    Correçao na conexao do bd

--- app/infra/databaseConnection.js
+++ app/infra/databaseConnection.js
@@ -1,29 +1,21 @@
-var mysql      = require('mysql');
+var mysql = require('mysql');
 var connection = {
-  host     : 'br182.hostgator.com.br',
-  user     : 'evidence_dev',
-  password : 'evidence_dev1',
-  database : 'evidence_artha'
+  host: 'br182.hostgator.com.br',
+  user: 'evidence_dev',
+  password: 'evidence_dev1',
+  database: 'evidence_artha'
 };
 
 function handleDisconnect() {
   connection = mysql.createConnection(connection);
-  
-  connection.connect(function(err) {
-    if(err) {
+
+  connection.connect(function (err) {
+    if (err) {
       console.error('error connecting: ' + err.stack);
       setTimeout(handleDisconnect, 2000);
     }
 
     console.log('connected as id ' + connection.threadId);
-  });
-
-  connection.on('error', function(err) {
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') {
-      handleDisconnect();
-    } else {
-      throw err;
-    }
   });
 }
 
