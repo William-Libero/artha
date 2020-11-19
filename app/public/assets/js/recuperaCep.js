474b091986ed13035ad9c2d787e0b8f4101ecb8b
William Libero <williamliberos@gmail.com>
2020-11-19 01:05:09 -0300

    Alteraçoes de names de inputs no form, inclusao da api de cep e integraçao da mesma com o front-end e alteraçao no back-end que alimenta a tabela de cadastro de pacientes

---
+++ app/public/assets/js/recuperaCep.js
@@ -0,0 +1,53 @@
+function recuperaCep(){
+    const cepDigitado = document.querySelector("#cep").value;
+    const url = `https://viacep.com.br/ws/${cepDigitado}/json/`;
+
+    fetch(url)
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        atribuiDadosDaApiDeCep(data)
+    })
+    .catch(err => {
+        console.log(err)
+    });
+}
+
+function atribuiDadosDaApiDeCep(data){
+    $('#estadoUf').html(data.uf);
+    $('#estadoUfPost').val(data.uf);
+    $('#cidade').html(data.localidade);
+    $('#cidadePost').val(data.localidade);
+    $('#bairro').html(data.bairro);
+    $('#bairroPost').val(data.bairro);
+    $('#endereco').html(data.logradouro); 
+    $('#enderecoPost').val(data.logradouro); 
+}
+
+function recuperaCepMedico(){
+    const cepDigitado = document.querySelector("#cep").value;
+    const url = `https://viacep.com.br/ws/${cepDigitado}/json/`;
+
+    fetch(url)
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        atribuiDadosDaApiDeCepMedico(data)
+    })
+    .catch(err => {
+        console.log(err)
+    });
+}
+
+function atribuiDadosDaApiDeCepMedico(data){
+    $('#estadoUfMedico').html(data.uf);
+    $('#estadoUfMedicoPost').val(data.uf);
+    $('#cidadeMedico').html(data.localidade);
+    $('#cidadeMedicoPost').val(data.localidade);
+    $('#bairroMedico').html(data.bairro);
+    $('#bairroMedicoPost').val(data.bairro);
+    $('#enderecoMedico').html(data.logradouro); 
+    $('#enderecoMedicoPost').val(data.logradouro); 
+}
