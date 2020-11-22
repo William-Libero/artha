6daf73fe525bb9b1deea885dd1f85d1cdc62e953
William Libero <williamliberos@gmail.com>
2020-11-21 22:57:46 -0300

    Inclusao da tabela de medico no arquivo de referencia do bd

--- banco-de-dados-Artha/db.sql
+++ banco-de-dados-Artha/db.sql
@@ -19,3 +19,26 @@ CREATE TABLE `usuario` (
   `senha` char(32) COLLATE latin1_general_ci DEFAULT NULL,
   PRIMARY KEY (`id_usuario`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+CREATE TABLE `medico` (
+  `id_medico` int(4) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(200) COLLATE latin1_general_ci NOT NULL,
+  `sobrenome` varchar(200) COLLATE latin1_general_ci NOT NULL,
+  `cpf` varchar(14) COLLATE latin1_general_ci DEFAULT NULL,
+  `rg` varchar(14) COLLATE latin1_general_ci DEFAULT NULL,
+  `dt_nasc` varchar(10) COLLATE latin1_general_ci DEFAULT NULL,
+  `telefone` varchar(15) COLLATE latin1_general_ci NOT NULL,
+  `crm` varchar(14) COLLATE latin1_general_ci DEFAULT NULL,
+  `ft_rg` varchar(14) COLLATE latin1_general_ci DEFAULT NULL,
+  `ft_crm` varchar(14) COLLATE latin1_general_ci DEFAULT NULL,
+  `email` varchar(200) COLLATE latin1_general_ci NOT NULL,
+  `endereco` varchar(200) COLLATE latin1_general_ci DEFAULT NULL,
+  `endereco_numero` varchar(8) COLLATE latin1_general_ci DEFAULT NULL,
+  `complemento` varchar(50) COLLATE latin1_general_ci DEFAULT NULL,
+  `bairro` varchar(200) COLLATE latin1_general_ci DEFAULT NULL,
+  `cidade` varchar(200) COLLATE latin1_general_ci DEFAULT NULL,
+  `estado_uf` varchar(2) COLLATE latin1_general_ci DEFAULT NULL,
+  `cep` varchar(9) COLLATE latin1_general_ci DEFAULT NULL,
+  `senha` char(32) COLLATE latin1_general_ci DEFAULT NULL,
+  PRIMARY KEY (`id_medico`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
