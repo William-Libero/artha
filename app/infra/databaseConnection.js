a9c541859bac1dd067482b65b6c1e4a925a2739b
William Libero <williamliberos@gmail.com>
2020-11-25 20:11:24 -0300

    Correçao no problema de conexao do bd

--- app/infra/databaseConnection.js
+++ app/infra/databaseConnection.js
@@ -15,6 +15,14 @@ function handleDisconnect() {
       setTimeout(handleDisconnect, 2000);
     }
 
+    connection.on('error', function(err) {
+      console.log('db error', err);
+      if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+        handleDisconnect();                         // lost due to either server restart, or a
+      } else {                                      // connnection idle timeout (the wait_timeout
+        throw err;                                  // server variable configures this)
+      }
+    });
     console.log('connected as id ' + connection.threadId);
   });
 }
